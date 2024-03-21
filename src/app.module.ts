e3dc270201ddb66ce7f2307087b67b0b48975f40
silasoli <silasoliv39@gmail.com>
2024-03-21 13:47:33 -0300

    feat: remove users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
@@ -8,7 +7,6 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    UsersModule,
   ],
   controllers: [AppController],
 })
