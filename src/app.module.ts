99eae419f66f15d43669ad742714e3dae8a977b9
silasoli <silasoliv39@gmail.com>
2024-03-21 13:27:19 -0300

    feat: adding pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     UsersModule,
