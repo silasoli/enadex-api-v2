9d9e53655330c514175031fd58cb13e1894ce925
silasoli <silasoliv39@gmail.com>
2024-05-04 21:32:12 -0300

    fix: adding find by id

--- src/courses/controllers/courses.controller.ts
+++ src/courses/controllers/courses.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { CoursesService } from '../services/courses.service';
 import { CreateCourseDto } from '../dto/create-course.dto';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CourseResponseDto } from '../dto/course-response.dto';
 import { ManagersRoleEnum } from '../../managers/schemas/manager.entity';
 import { Role } from '../../roles/decorators/roles.decorator';
+import { IDQueryDTO } from '../../common/dto/id-query.dto';
+import { AuthUserJwtGuard } from '../../auth/guards/auth-user-jwt.guard';
+import { RoleGuard } from '../../roles/guards/role.guard';
 
+@ApiBearerAuth()
 @ApiTags('Courses')
 @Controller('courses')
+@UseGuards(AuthUserJwtGuard, RoleGuard)
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
@@ -24,15 +35,26 @@ export class CoursesController {
     return this.coursesService.create(dto);
   }
 
-  @ApiOperation({ summary: 'Obter listagem de cursos dos usuários' })
+  @ApiOperation({ summary: 'Obter listagem de cursos' })
   @ApiResponse({
     status: 200,
     description: 'Listagem de cursos retornada com sucesso',
     type: CourseResponseDto,
   })
-  @Role([ManagersRoleEnum.COORDINATORS])
   @Get()
   findAll(): Promise<CourseResponseDto[]> {
     return this.coursesService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Obter curso' })
+  @ApiResponse({
+    status: 200,
+    description: 'Curso retornado com sucesso',
+    type: CourseResponseDto,
+  })
+  @Role([ManagersRoleEnum.COORDINATORS])
+  @Get(':id')
+  findOne(@Param() params: IDQueryDTO): Promise<CourseResponseDto> {
+    return this.coursesService.findOne(params.id);
   }
 }
