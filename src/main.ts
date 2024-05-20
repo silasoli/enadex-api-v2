95bf876da190499a1e8341fce976f5aea2715ac2
silasoli <silasoliv39@gmail.com>
2024-05-20 19:05:18 -0300

    docs: change theme of swagger

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,9 @@ async function bootstrap() {
     customfavIcon:
       'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.0/favicon-32x32.png',
     customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+      // 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+      //https://www.jsdelivr.com/package/npm/swagger-themes
+      'https://cdn.jsdelivr.net/npm/swagger-themes@1.4.3/themes/dark.min.css',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
