2aeb11b5373469e2de1a0baae89fb8aba7769bb7
silasoli <silasoliv39@gmail.com>
2024-05-26 13:27:24 -0300

    docs: update api version
    Co-authored-by: Gustavo <gustavosg91@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Enadex API')
     .setDescription('Enadex API developed by ENADEX')
-    .setVersion('1.24.15')
+    .setVersion('1.25.16')
     .addBearerAuth()
     .build();
 
