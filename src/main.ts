c98de4bb13081b579b5955889dfd441bf6ae20fc
silasoli <silasoliv39@gmail.com>
2024-06-02 18:06:49 -0300

    feat: update version api

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Enadex API')
     .setDescription('Enadex API developed by ENADEX')
-    .setVersion('1.28.20')
+    .setVersion('1.29.20')
     .addBearerAuth()
     .build();
 
