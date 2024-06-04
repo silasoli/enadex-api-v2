9f0dfbdb8e6d75a28a0e859e4d30154d081380a7
silasoli <silasoliv39@gmail.com>
2024-06-04 17:17:48 -0300

    feat: adding dark theme in swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/exception-filters/http-exception.filter';
 import { MongoExceptionFilter } from './common/exception-filters/mongo-exception.filter';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +30,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  const theme = new SwaggerTheme();
 
   SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
@@ -38,8 +40,9 @@ async function bootstrap() {
     },
     customfavIcon:
       'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.0/favicon-32x32.png',
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    // customCssUrl:
+    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
